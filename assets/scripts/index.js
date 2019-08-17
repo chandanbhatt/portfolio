15c037f80370a5ea3eab1444a0543a6bd44066f1
chandan <bhattchandanbhatt2580@gmail.com>
2019-08-17 15:29:27 +0530

    navbar complete

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,39 +1,66 @@
 var chia = (function () {
     let loading = document.getElementById('loading-icon');
+    let navbarToggleButton = document.getElementsByClassName('navbar__toggler');
+
+    let init = function () {
+        Array.prototype.forEach.call(navbarToggleButton, (el) => {
+            el.addEventListener('click', (e) => {
+                toggleMenu(el);
+            })
+        });
+    }
     let stopLoadingAnimation = function () {
         loading.classList.add('hide-item');
     }
     let startLoadingAnimation = function () {
         loading.classList.remove('show-item');
     }
-    let toggleMenu = function (el) {
-        let target = document.getElementById(el.dataset.target);
-        let toggleStatus = el.dataset.toggle;
-        if(toggleStatus === 'collapse'){
-            el.dataset.toggle = 'grow';
-            target.style.maxHeight = '200px';
+    let toggleMenu = function (el, isSizeChange = false, size = 576) {
+        let target, toggleStatus;
+        if (el && !isSizeChange) {
+            target = document.getElementById(el.dataset.target);
+            toggleStatus = el.dataset.toggle;
+            if (toggleStatus === 'collapse') {
+                el.dataset.toggle = 'grow';
+                target.style.height = '100%';
+                target.style.maxHeight = '200px';
+            } else {
+                el.dataset.toggle = 'collapse';
+                target.style.maxHeight = 0;
+            }
         } else {
-            el.dataset.toggle = 'collapse';
-            target.style.maxHeight = 0;
+            Array.prototype.forEach.call(navbarToggleButton, (ele) => {
+                target = document.getElementById(ele.dataset.target);
+                if (size > 992) {
+                    target.style.height = '100%';
+                    target.style.maxHeight = '200px';
+                } else {
+                    ele.dataset.toggle = 'collapse';
+                    target.style.height = 0;
+                    target.style.maxHeight = 0;
+                }
+            });
         }
+
     }
     return {
         stopLoadingAnimation: stopLoadingAnimation,
         startLoadingAnimation: startLoadingAnimation,
+        init: init,
         toggleMenu: toggleMenu
     }
 })();
 
-window.addEventListener('DOMContentLoaded', (event)=>{
-    setTimeout(()=>{
+window.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(() => {
         chia.stopLoadingAnimation();
-    },500);
+    }, 1000);
 
-    let navbarToggleButton = document.getElementsByClassName('navbar-toggler');
-    Array.prototype.forEach.call(navbarToggleButton,(el)=>{
-       el.addEventListener('click', (e)=>{
-           chia.toggleMenu(el);
-       })
-    });
+    chia.init();
+
+    window.addEventListener('resize', () => {
+        let screenSize = window.innerWidth;
+        chia.toggleMenu(null, true, screenSize);
+    })
 
 });
